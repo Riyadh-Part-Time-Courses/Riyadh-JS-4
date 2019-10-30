45ad78f7a7d3502fe63f5137face5b67c451197d
Yahya Aloyoni <39294093+YA7MO@users.noreply.github.com>
2019-10-30 21:22:13 +0300

    Added DOM exercise

---
+++ 07-intro-to-dom-and-jquery/homework/script.js
@@ -0,0 +1,18 @@
+console.log("JS connected"); // this to check that our script is connected to out HTML
+
+var body = "";// query select body 
+// change the font family of the body to 'Arial, sans-serif' Hint: use .style to acess CSS properties
+// change the backgroundColor to a color of your choice
+
+var nickname =  "";// qurey select the ID nickname
+//add some value to nickname Hint: innerHTML
+
+// query select favorites byID
+// query select hometown byID
+// feed both favorites and homwtown with values that represents you i.e hometown.innerHTML = 'Riyadh';
+
+//change the color of all selectors to colors of your choice, it better be good LOL
+
+
+
+// https://www.w3schools.com/jsref/prop_style_color.asp
